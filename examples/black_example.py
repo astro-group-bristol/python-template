b03d317c6d7614b3317cc1463e079a0d34a9fa5f
phajy <andy.young@bristol.ac.uk>
2023-12-08 16:36:07 +0000

    fixed

--- examples/black_example.py
+++ examples/black_example.py
@@ -1,27 +1,32 @@
 """Here are some examples of code that make me sad."""
 
-def my_function(   x):
-    if x   > 10:
+
+def my_function(x):
+    if x > 10:
         print("x is greater than 10")
-        else:
-            print("x is not greater than 10")
+    else:
+        print("x is not greater than 10")
 
 
 def calculate_sum(a, b):
-    result=a+b
+    result = a + b
     return result
+
 
 x = 10
 y = 5
 
-sum_result= calculate_sum(x, y)
-print("The sum is: " sum_result)
+sum_result = calculate_sum(x, y)
+print("The sum is: ", sum_result)
 
 
-def ThisIsMyJankyFunction:
+def ThisIsMyJankyFunction():
     """Include doctstrings in your functions."""
-    13_is_a_bad_variable_name = 13
+    thirteen_is_a_bad_variable_name = 13
 
-    try_using_single_quotes = 'this is a string'
+    try_using_single_quotes = "this is a string"
 
-    return 13_is_a_bad_variable_name, try_using_single_quotes
+    return thirteen_is_a_bad_variable_name, try_using_single_quotes
+
+
+"""random change"""
